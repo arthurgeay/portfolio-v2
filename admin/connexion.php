d7f0c006e0e3bdf4dea858d9dae37b7ef39f75c9
arthurgeay <arthurgeay.contact@gmail.com>
2019-05-05 18:30:01 +0200

    Réorganisation des fichiers CSS + début de création de la page connexion

---
+++ admin/connexion.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+	  	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	  	<link rel="stylesheet" href="../css/admin.css" />
+	  	<link rel="stylesheet" href="../css/main.css" />
+		<title>Connexion à l'administration</title>
+	</head>
+	<body>
+		<main class="login">
+			<h1>Connexion</h1>
+			<hr>
+			<form>
+				<input type="text" placeholder="Identifiant" required />
+				<input type="password" placeholder="Mot de passe" required />
+				<input type="submit" />
+			</form>
+		</main>
+
+	</body>
+</html>
